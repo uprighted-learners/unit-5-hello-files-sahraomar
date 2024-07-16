9c73f6a968a4d524ccdcccd85ef18e66983e7323
sahraomar <sahraaliwrites@gmail.com>
2024-07-15 20:52:07 -0400

    commit 1

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/page1", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "page1.html"));
+});
+
+app.get("/page2", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "page2.html"));
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
 });
